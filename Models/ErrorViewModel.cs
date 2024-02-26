283e5f2fccff0479dd3f94fd1ca7f7de8d6cabe7
Gaetano <gaem195@gmail.com>
2024-02-26 16:12:58 +0100

    Aggiungere i file di progetto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace eserS5L1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
